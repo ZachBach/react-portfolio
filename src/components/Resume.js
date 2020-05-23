a799f7e3430b733d6dcbb5a7ede9e9d5636e44da
Zach Auerbach <auernachzach@gmail.com>
2020-05-22 21:33:53 -0700

    Converted my portfolio from HTML to JSX

---
+++ src/components/Resume.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <div>
+      <div className='container'>
+        <img
+          src={'assets/Images/Dev-resume.png'}
+          alt={'...'}
+          //   style={{ height: '1000px', width: '1000px' }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Resume;
